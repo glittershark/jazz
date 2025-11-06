b5b11b16062df8d94345d5cd6350ef7758600508
Aspen Smith <root@gws.fyi>
2025-11-05 23:04:43 -0500

    reverse: Add feedback knob

--- src/reverse/reverse.cpp
+++ src/reverse/reverse.cpp
@@ -1,5 +1,6 @@
 #include "daisy_seed.h"
 #include "daisysp.h"
+#include "hid/logger.h"
 
 using namespace daisy;
 using namespace daisysp;
@@ -7,9 +8,9 @@ using namespace daisysp;
 DaisySeed hw;
 
 #define BUFFER_LEN 44100 * 2 /* 2 seconds */
-#define FEEDBACK 0.3
 
 static int delay_samples = 22000;
+static int feedback = 0.3;
 static float buffer[BUFFER_LEN] = {};
 static int read_pos = BUFFER_LEN;
 static int write_pos = 0;
@@ -17,13 +18,13 @@ static int write_pos = 0;
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
                    size_t size) {
   for (size_t i = 0; i < size; i++) {
-    auto sample = in[0][i] * 0.5;
+    auto sample = in[0][i] * 0.9;
     read_pos = read_pos - 1;
     if (read_pos == -1) {
       read_pos = delay_samples - 1;
     }
     auto delayed_signal = buffer[read_pos];
-    buffer[write_pos] = sample + (delayed_signal * FEEDBACK);
+    buffer[write_pos] = sample + (delayed_signal * feedback);
     write_pos = (write_pos + 1) % (delay_samples);
 
     out[0][i] = sample + delayed_signal;
@@ -35,14 +36,17 @@ int main(void) {
   hw.Init();
   hw.SetAudioBlockSize(4);
 
-  AdcChannelConfig adcConfig;
-  adcConfig.InitSingle(hw.GetPin(21));
-  hw.adc.Init(&adcConfig, 1);
+  AdcChannelConfig adcConfig[2];
+  adcConfig[0].InitSingle(hw.GetPin(21)); /* delay amount */
+  adcConfig[1].InitSingle(hw.GetPin(20)); /* feedback */
+  hw.adc.Init(adcConfig, 2);
   hw.adc.Start();
 
   hw.StartAudio(AudioCallback);
+  hw.PrintLine("again");
   for (;;) {
     delay_samples = (int)(hw.adc.GetFloat(0) * BUFFER_LEN);
-    System::Delay(1);
+    auto new_feedback = hw.adc.GetFloat(1);
+    feedback = new_feedback;
   }
 }
