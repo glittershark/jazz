e876433b06b749ae6821a975c2d963f7dfd59b56
Aspen Smith <root@gws.fyi>
2025-11-12 22:23:15 -0500

    Install jupyter packages in shel.nix

--- shell.nix
+++ shell.nix
@@ -3,5 +3,14 @@
 with pkgs;
 
 mkShell {
-  buildInputs = [ python3 gcc-arm-embedded dfu-util bear uucp cmake gtest ];
+  buildInputs = [
+    (python3.withPackages
+      (p: with p; [ ipython matplotlib jupyter jupyterlab notebook tqdm ]))
+    gcc-arm-embedded
+    dfu-util
+    uucp
+    cmake
+    gtest
+    rapidcheck
+  ];
 }
