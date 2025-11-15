aee368626ea07338b26b94ffa29cc3e2adb8f121
Aspen Smith <root@gws.fyi>
2025-11-15 13:28:56 -0500

    granular: Representation of buffer elements
    Every buffer element has a sample, and any number of updates. These are
    intrusive linked lists allocated in slabs

--- src/granular/granular.cpp
+++ src/granular/granular.cpp
@@ -1,6 +1,9 @@
+#include <optional>
+
 #include "daisy_seed.h"
 #include "daisysp.h"
 #include "hid/logger.h"
+#include "libjazz/slab.hpp"
 #include "libjazz/value.hpp"
 
 using namespace daisy;
@@ -9,15 +12,60 @@ using namespace daisysp;
 DaisySeed hw;
 
 struct Update {
-  enum { erase, write } kind;
+  enum Kind { sample, erase, write } kind;
   size_t finished_at;
+  float value;
   size_t samples;
+  std::optional<Update *> next;
 };
+
+static Slab<Update, 2048> UPDATES;
 
 #define BUFFER_LEN 44100 * 2 /* 2 seconds */
 #define MAX_FADE_TIME 10000
 
-static Value buffer[BUFFER_LEN];
+class BufferValue : private Value {
+public:
+  struct SampleWithUpdates {
+    float sample;
+    Update *first_update;
+  };
+  static Slab<SampleWithUpdates, BUFFER_LEN> SAMPLES;
+
+protected:
+  SampleWithUpdates *asSampleWithUpdates() const {
+    return std::bit_cast<SampleWithUpdates *>(getPointer());
+  }
+  float asSample() const { return getFloat(); }
+
+public:
+  BufferValue(float sample) : Value(sample) {}
+  BufferValue() : BufferValue(0.0f) {}
+
+  Update *PushBack(Update &&update) {
+    if (isPointer()) {
+      Update *cur = asSampleWithUpdates()->first_update;
+      while (!cur->next.has_value()) {
+        cur = cur->next.value();
+      }
+      cur->next = UPDATES.Alloc(std::move(update));
+      return cur->next.value();
+    } else {
+      float sample = asSample();
+      auto upd = UPDATES.Alloc(std::move(update));
+      auto head = SAMPLES.Alloc(sample, upd);
+      new (this) Value((void *)head);
+      return upd;
+    }
+  }
+};
+
+static BufferValue buffer[BUFFER_LEN];
+
+void Erase(size_t index, size_t clock_time, float value,
+           size_t samples = MAX_FADE_TIME) {
+  // buffer[index]
+}
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
                    size_t size) {
