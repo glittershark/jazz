b25946e05af7d5b68fd5e425889f591a27af2791
Rachel Inman <rinman@princeton.edu>
2025-11-19 22:59:26 -0500

    Rachel effect

---
+++ src/rachel/rachel.cpp
@@ -0,0 +1,61 @@
+#include "daisy_seed.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace daisysp;
+
+Oscillator lfo;  // LFO for modulation
+DaisySeed hw;
+
+#define BUFFER_LEN 44100 * 2
+#define FEEDBACK 0.3
+
+static float buffer[BUFFER_LEN] = {};
+static int buffer_pos = 0;
+static float base_delay_samples = 22000;
+static float sample_rate;
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
+                   size_t size) {
+  for (size_t i = 0; i < size; i++) {
+    float lfo_val = lfo.Process();
+    
+    float delay_mod = base_delay_samples * (1.0f + lfo_val * 0.1f); // 10% modulation
+    int delay_samples = (int)delay_mod;
+    
+    int read_pos = (buffer_pos - delay_samples + BUFFER_LEN) % BUFFER_LEN;
+    
+    auto sample = in[0][i] / 2.0f;
+    auto delayed_signal = buffer[read_pos];
+    
+    buffer[buffer_pos] = sample + (delayed_signal * FEEDBACK);
+    buffer_pos = (buffer_pos + 1) % BUFFER_LEN;
+    
+    out[0][i] = sample + delayed_signal;
+  }
+}
+
+int main(void) {
+  hw.Configure();
+  hw.Init();
+  hw.SetAudioBlockSize(4);
+  sample_rate = hw.AudioSampleRate();
+  
+  // Initialize LFO (slow oscillation, like 0.5-2 Hz)
+  lfo.Init(sample_rate);
+  lfo.SetFreq(1.0f);  // 1 Hz oscillation
+  lfo.SetWaveform(Oscillator::WAVE_SIN);
+  lfo.SetAmp(1.0f);
+  
+  AdcChannelConfig adcConfig;
+  adcConfig.InitSingle(hw.GetPin(21));
+  hw.adc.Init(&adcConfig, 1);
+  hw.adc.Start();
+  
+  hw.StartAudio(AudioCallback);
+  for (;;) {
+    // Control base delay time with knob
+    base_delay_samples = hw.adc.GetFloat(0) * BUFFER_LEN;
+    System::Delay(1);
+  }
+}
