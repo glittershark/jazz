dc469437d34a7deeace209f72bc1a172f6c6a3a4
Aspen Smith <root@gws.fyi>
2025-11-19 23:59:05 -0500

    three rachels! wow

--- src/rachel/rachel.cpp
+++ src/rachel/rachel.cpp
@@ -7,12 +7,13 @@ using namespace daisysp;
 Oscillator lfo; // LFO for modulation
 DaisySeed hw;
 
-#define BUFFER_LEN 44100
+#define BUFFER_LEN 22000
 #define FEEDBACK 0.9
 
-static float buffer[BUFFER_LEN][2] = {{}, {}};
-static int buffer_pos[] = {0, 0};
-static int delay_samples[] = {12000, 2200};
+constexpr const size_t nbuffers = 3;
+static float buffer[BUFFER_LEN][nbuffers];
+static int buffer_pos[] = {0, 0, 0};
+static int delay_samples[] = {12000, 2200, 3333};
 static float sample_rate;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
@@ -20,9 +21,9 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
   for (size_t i = 0; i < size; i++) {
     float lfo_val = lfo.Process();
 
-    auto sample = in[0][i] / 2.0f;
+    auto sample = in[0][i] * 0.3f;
     float o = sample;
-    for (size_t buf = 0; buf < 1; buf++) {
+    for (size_t buf = 0; buf < nbuffers; buf++) {
 
       float delay_mod =
           delay_samples[buf] * (1.0f + lfo_val * 0.5f); // 10% modulation
@@ -39,7 +40,7 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
       o += delayed_signal;
     }
     if (o > 0.5) {
-      o *= 0.8;
+      o = 0.5 + 0.8 * (o - 0.5);
     }
 
     out[0][i] = o;
