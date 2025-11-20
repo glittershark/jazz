e9817a019ff1251b6609d91490513860fab6430f
Aspen Smith <root@gws.fyi>
2025-11-19 23:47:32 -0500

    if you liked one rachel wait til you hear 2 rachel

--- src/rachel/rachel.cpp
+++ src/rachel/rachel.cpp
@@ -7,12 +7,12 @@ using namespace daisysp;
 Oscillator lfo; // LFO for modulation
 DaisySeed hw;
 
-#define BUFFER_LEN 44100 * 2
-#define FEEDBACK 0.3
+#define BUFFER_LEN 44100
+#define FEEDBACK 0.9
 
-static float buffer[BUFFER_LEN] = {};
-static int buffer_pos = 0;
-static int delay_samples = 22000;
+static float buffer[BUFFER_LEN][2] = {{}, {}};
+static int buffer_pos[] = {0, 0};
+static int delay_samples[] = {12000, 2200};
 static float sample_rate;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
@@ -20,20 +20,29 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out,
   for (size_t i = 0; i < size; i++) {
     float lfo_val = lfo.Process();
 
-    float delay_mod = delay_samples * (1.0f + lfo_val * 0.1f); // 10% modulation
-    int delay_samples = (int)delay_mod;
-
-    int read_pos = buffer_pos % (delay_samples - 1);
-
     auto sample = in[0][i] / 2.0f;
-    auto delayed_signal = buffer[read_pos];
+    float o = sample;
+    for (size_t buf = 0; buf < 1; buf++) {
 
-    buffer[buffer_pos] = sample + (delayed_signal * FEEDBACK);
-    buffer_pos = (buffer_pos + 1) % (delay_samples - 1
-                                     // BUFFER_LEN
-                                    );
+      float delay_mod =
+          delay_samples[buf] * (1.0f + lfo_val * 0.5f); // 10% modulation
+      int delay_samples = (int)delay_mod;
 
-    out[0][i] = sample + delayed_signal;
+      int read_pos = buffer_pos[buf] % (delay_samples - 1);
+
+      auto delayed_signal = buffer[buf][read_pos];
+
+      buffer[buf][buffer_pos[buf]] = sample + (delayed_signal * FEEDBACK);
+      buffer_pos[buf] = (buffer_pos[buf] + 1) % (delay_samples - 1
+                                                 // BUFFER_LEN
+                                                );
+      o += delayed_signal;
+    }
+    if (o > 0.5) {
+      o *= 0.8;
+    }
+
+    out[0][i] = o;
   }
 }
 
