286be28ad40654b213f42d5d092338d937e32fae
Nausicaä Van West <root@khz.ac>
2026-04-12 18:13:24 -0400

    fridge/led: draft controller

--- src/fridge/led.cpp
+++ src/fridge/led.cpp
@@ -1,1 +1,53 @@
 #include "led.hpp"
+
+#ifndef UNIT_TEST
+
+using namespace fridge::driver::led;
+
+#include <cassert>
+
+#include "daisy_seed.h"
+
+Controller::Controller(uint16_t address) : address(address), timeout(1000) {
+  I2CHandle::Config c;
+  c.periph = I2CHandle::Config::Peripheral::I2C_1;
+  c.mode = I2CHandle::Config::Mode::I2C_MASTER;
+  c.speed = I2CHandle::Config::Speed::I2C_400KHZ;
+  c.pin_config = {
+      .scl = daisy::seed::D11,
+      .sda = daisy::seed::D12,
+  };
+
+  assert(i2c.Init(c) == I2CHandle::Result::OK);
+
+  // TODO(nausicaa): chip-specific register initialization
+}
+
+Address Controller::Led::OnAddress() {
+  return 0x00 + (y << 1) + (matrix == Matrix::A ? 0 : 1);
+}
+
+uint8_t Controller::Led::OnOffset() {
+  return x;  // lol
+}
+
+Address Controller::Led::PwmAddress() {
+  return 0x24 + x + (0x10 * y) + (matrix == Matrix::A ? 0 : 0x08);
+}
+
+void Controller::Led::On(bool on) {
+  uint8_t row;
+
+  c.i2c.ReadDataAtAddress(c.address, OnAddress(), 1, &row, 1, c.timeout);
+
+  uint8_t bit = 1 << OnOffset();
+  row = on ? row | bit : row & ~bit;
+
+  c.i2c.WriteDataAtAddress(c.address, OnAddress(), 1, &row, 1, c.timeout);
+}
+
+void Controller::Led::Pwm(uint8_t duty) {
+  c.i2c.WriteDataAtAddress(c.address, PwmAddress(), 1, &duty, 1, c.timeout);
+}
+
+#endif  // UNIT_TEST
--- src/fridge/led.hpp
+++ src/fridge/led.hpp
@@ -1,8 +1,67 @@
 #ifndef LED_H_
 #define LED_H_
 
-#include "daisy_seed.h"
+#ifndef UNIT_TEST
 
-// driver for the IS31FL3731
+#include <cstdint>
+
+#include "per/i2c.h"
+
+namespace fridge::driver::led {
+
+using daisy::I2CHandle;
+
+using Address = uint16_t;
+
+/**
+ * Driver for the IS31FL3731.
+ *
+ * A note on addressing: the datasheet uses a scheme (per-matrix) like C_{y-x},
+ * so if you're referencing the datasheet, that's how to interpret the x-y
+ * address pairs.
+ */
+class Controller {
+  enum class Matrix {
+    A,
+    B,
+  };
+
+ public:
+  class Led {
+    friend class Controller;
+
+    Controller& c;
+    Matrix matrix;
+    uint8_t x;
+    uint8_t y;
+
+    // note: private constructor!
+    Led(Controller& c, Matrix matrix, uint8_t x, uint8_t y)
+        : c(c), matrix(matrix), x(x), y(y) {}
+
+    Address OnAddress();
+    uint8_t OnOffset();
+
+    Address PwmAddress();
+
+   public:
+    void On(bool on);
+    void Pwm(uint8_t duty);
+  };
+
+  Controller(uint16_t address = 0x75);
+
+  Led A(uint8_t x, uint8_t y) { return Led(*this, Matrix::A, x, y); };
+  Led B(uint8_t x, uint8_t y) { return Led(*this, Matrix::B, x, y); };
+
+ private:
+  I2CHandle i2c;
+  uint16_t address;
+  uint32_t timeout;
+};
+
+}  // namespace fridge::driver::led
+
+#endif  // UNIT_TEST
 
 #endif  // LED_H_
