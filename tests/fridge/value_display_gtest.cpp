09d1334cd1ce80856a45b8b54aca48e691fa96f6
Aspen Smith <root@gws.fyi>
2026-04-23 00:14:19 -0400

    Linear interpolation in CIE color space for value display
    Including a very pretty blue-to-green gradient

--- tests/fridge/value_display_gtest.cpp
+++ tests/fridge/value_display_gtest.cpp
@@ -29,4 +29,29 @@ TEST(HueWheelTest, Scaling) {
   EXPECT_NEAR(res.hue, 75, 1);
 }
 
+TEST(CieInterpTest, Interpolation) {
+  ui::value_display::CieInterp interp{
+      .start = color::XYZ(0, 100, 200),
+      .end = color::XYZ(100, 200, 50),
+  };
+
+  // At 0, should return start
+  color::XYZ res = interp(0);
+  EXPECT_EQ(res.x, 0);
+  EXPECT_EQ(res.y, 100);
+  EXPECT_EQ(res.z, 200);
+
+  // At 255, should return end
+  res = interp(255);
+  EXPECT_EQ(res.x, 100);
+  EXPECT_EQ(res.y, 200);
+  EXPECT_EQ(res.z, 50);
+
+  // At 128, should return midpoint (approximately)
+  res = interp(128);
+  EXPECT_NEAR(res.x, 50, 1);
+  EXPECT_NEAR(res.y, 150, 1);
+  EXPECT_NEAR(res.z, 125, 1);
+}
+
 }  // namespace
